fa67510854267eb75e8fe7512bd39d90f63825f6
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2023-01-03 23:13:18 +0600

    home page

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    mode:'production',
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+};
